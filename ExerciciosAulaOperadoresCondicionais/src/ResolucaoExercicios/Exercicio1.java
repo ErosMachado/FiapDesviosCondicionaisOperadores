2bab8777288de419dd00990295896d776a0aa7eb
ErosMachado <erosrodriguesmachado@gmail.com>
2024-08-29 14:39:52 -0300

    commit 1.0

---
+++ ExerciciosAulaOperadoresCondicionais/src/ResolucaoExercicios/Exercicio1.java
@@ -0,0 +1,15 @@
+package ResolucaoExercicios;
+import java.util.Scanner; 
+
+public class Exercicio1 {
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in); 
+		
+		System.out.println("Digite um numero: ");
+		int num = scan.nextInt();
+		
+		
+		scan.close();
+	}
+}
