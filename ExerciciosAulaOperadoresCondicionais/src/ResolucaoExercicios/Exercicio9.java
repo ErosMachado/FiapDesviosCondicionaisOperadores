7450043f1ef1e94876ece1293b102f9956b20488
Eros Machado <112194183+ErosMachado@users.noreply.github.com>
2024-08-30 23:46:24 -0300

    Update Exercicio9.java
    Resolução do Exercicio9

--- ExerciciosAulaOperadoresCondicionais/src/ResolucaoExercicios/Exercicio9.java
+++ ExerciciosAulaOperadoresCondicionais/src/ResolucaoExercicios/Exercicio9.java
@@ -1,10 +1,41 @@
 package ResolucaoExercicios;
+import java.util.Scanner;
 
 public class Exercicio9 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	    Scanner scan = new Scanner(System.in);
 
+        System.out.println("Informe o tipo de cliente:");
+        System.out.println("1 - Residência");
+        System.out.println("2 - Comércio");
+        System.out.println("3 - Indústria");
+        int tC = scan.nextInt();
+
+        System.out.print("Informe o consumo em kWh: ");
+        double consumo = scan.nextDouble();
+
+        double valor;
+        switch (tC) {
+            case 1:
+                valor = 0.60;
+                break;
+            case 2:
+                valor = 0.48;
+                break;
+            case 3:
+                valor = 1.29;
+                break;
+            default:
+                System.out.println("Tipo de cliente inválido!");
+                return; 
+        }
+
+        double Total = consumo * valor;
+
+        System.out.printf("O valor da conta de luz é: "+ Total);
+
+        scan.close();
 	}
-
 }
+//fim
