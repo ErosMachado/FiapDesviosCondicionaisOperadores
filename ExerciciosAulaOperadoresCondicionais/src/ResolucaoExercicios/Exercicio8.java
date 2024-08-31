4c24173aa4b818663492dd356559371669467a77
Eros Machado <112194183+ErosMachado@users.noreply.github.com>
2024-08-30 23:38:16 -0300

    Update Exercicio8.java
    exercicio8 resolvido

--- ExerciciosAulaOperadoresCondicionais/src/ResolucaoExercicios/Exercicio8.java
+++ ExerciciosAulaOperadoresCondicionais/src/ResolucaoExercicios/Exercicio8.java
@@ -5,8 +5,20 @@ public class Exercicio8 {
 
 	public static void main(String[] args) {
 
-		Scanner scan = new Scanner(System.in); 
+	Scanner scanner = new Scanner(System.in);
+        System.out.print("Informe a distância total percorrida (em km): ");
+        double distancia = scanner.nextDouble();
+        System.out.print("Informe o total de combustível gasto (em litros): ");
+        double combustivel = scanner.nextDouble();
+        double consumoMedio = distancia / combustivel;
+        System.out.printf("Consumo médio: %.2f km/L\n", consumoMedio);
+        if (consumoMedio < 8) {
+            System.out.println("Esse carro bebe hein!");
+        } else {
+            System.out.println("Autonomia legal");
+        }
 
-		scan.close();
+        scanner.close();
 	}
 }
+
