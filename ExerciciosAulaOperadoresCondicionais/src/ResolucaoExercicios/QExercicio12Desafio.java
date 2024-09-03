a923a326d02a46fa9b5b96f546987290977e7131
ErosMachado <erosrodriguesmachado@gmail.com>
2024-09-03 14:16:53 -0300

    teste

--- ExerciciosAulaOperadoresCondicionais/src/ResolucaoExercicios/QExercicio12Desafio.java
+++ ExerciciosAulaOperadoresCondicionais/src/ResolucaoExercicios/QExercicio12Desafio.java
@@ -1,11 +1,10 @@
 package ResolucaoExercicios;
-import java.util.Scanner;
-
 
 public class QExercicio12Desafio {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
 	}
-}
-//teste
+
+}//
